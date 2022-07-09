042cc0d40337859d39b36f4a5b40b57e6c28907c
vicky <s6102161021@gmail.com>
2022-07-09 19:30:42 +0800

    7-初版完成

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ let checkBtn = document.getElementById('checkBtn');
 let inputHeight = document.getElementById('inputHeight');
 let inputWeight = document.getElementById('inputWeight');
 let list = document.querySelector('.main__list');
-let data = JSON.parse(localStorage.getItem('data')) || [];
+let data = JSON.parse(localStorage.getItem('bmiData')) || [];
 
 function countBMI() {
   let bmi = inputWeight.value / [(inputHeight.value/100) * (inputHeight.value/100)];
   bmi = parseFloat(bmi.toFixed(2));
   let className;
-  let span;
+  let result;
   let classNameBg;
 
   if (inputHeight.value === '') {
@@ -23,32 +23,32 @@ function countBMI() {
 
   if(bmi<18.5){
     className = 'btnUnderweight';
-    span = '過輕';
+    result = '過輕';
     classNameBg = 'underweight';
   }else if(18.5<=bmi && bmi<25){
     className = 'btnNormal';
-    span = '理想';
+    result = '理想';
     classNameBg = 'normal';
   }else if(25 <= bmi && bmi < 30){
     className = 'btnOverweight';
-    span = '過重';
+    result = '過重';
     classNameBg = 'overweight';
   }else if(30 <= bmi && bmi <35){
     className = 'btnObese1';
-    span = '輕度肥胖';
+    result = '輕度肥胖';
     classNameBg = 'obese1';
   }else if(35 <= bmi && bmi < 40){
     className = 'btnObese2';
-    span = '中度肥胖';
+    result = '中度肥胖';
     classNameBg = 'obese2';
   }else{
     className = 'btnObese3';
-    span = '重度肥胖';
+    result = '重度肥胖';
     classNameBg = 'obese3';
   }
   checkBtn.innerHTML = `
   <div class="header__btn btn btnResule ${className}"> 
-      <span>${span}</span>
+      <span>${result}</span>
       <p>${bmi}</p>
       <sup>BMI</sup>
       <div class="${classNameBg}"></div>
@@ -56,36 +56,42 @@ function countBMI() {
 
   let height = inputHeight.value;
   let weight = inputWeight.value;
-  let bmiList ={bmiH: height, bmiW: weight};
+  let now =new Date();
+  let time = now.getDate() + '-' + (now.getMonth() + 1) + '-' + now.getFullYear();
+  let bmiList ={
+    result: result, 
+    bmi: bmi, 
+    height: height, 
+    weight: weight,
+    classNameBg: classNameBg,
+    time: time
+  };
   data.push(bmiList);
-  updateList(data);
-  height.value = '';
-  weight.value = '';
+  updateList();
   localStorage.setItem('bmiData', JSON.stringify(data));
 }
 checkBtn.addEventListener('click', countBMI);
-updateList(data);
+updateList();
 
-function updateList(data){
+function updateList(){
   let str = '';
   for (let i = 0; i < data.length; i++) {
     let content = `<li class="card">
             <ul class="card__note">
               <li class="card__note__result">
-                <div class="normal"></div>
-                <p>a</p>
+                <div class="${data[i].classNameBg}"></div>
+                <p>${data[i].result}</p>
               </li>
-              <li><sup>BMI</sup><span>b</span></li>
-              <li><sup>weight</sup><span>${data[i].bmiW}kg</span></li>
-              <li><sup>height</sup><span>cm</span></li>
-              <li><sup>06-19-2017</sup></li>
+              <li><sup>BMI</sup><span>${data[i].bmi}</span></li>
+              <li><sup>weight</sup><span>${data[i].weight}kg</span></li>
+              <li><sup>height</sup><span>${data[i].height}cm</span></li>
+              <li><sup>${data[i].time}</sup></li>
             </ul>
           </li>`;
     str += content;
   }
   list.innerHTML = str;
 }
-
 
 //toTop
 $(function () {
